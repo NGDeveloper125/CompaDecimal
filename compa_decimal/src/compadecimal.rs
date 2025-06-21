10226cde0bb23d1382237a800c3bb6d2ef7341a0
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-06-21 10:48:15 +0100

    Add biguint dependencies

--- compa_decimal/src/compadecimal.rs
+++ compa_decimal/src/compadecimal.rs
@@ -1,4 +1,5 @@
-use num::{PrimInt, Unsigned};
+use num::{PrimInt, Unsigned, Zero, One, ToPrimitive};
+use num_bigint::BigUint;
 use std::{any::type_name_of_val, fmt::Display, str::FromStr};
 
 use crate::{error::*, utils::*};
